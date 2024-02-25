31dd387965bf03a8e0649549bd9dbf5b63fb7082
luis.otavio.santos <luis.otavio.santos@posgrad.ufsc.br>
2024-02-25 19:44:47 -0300

    get_colormaps fix

--- lapixdl/evaluation/visualize.py
+++ lapixdl/evaluation/visualize.py
@@ -10,6 +10,7 @@ from matplotlib import cm
 from matplotlib.axes import Axes
 from matplotlib.colors import Colormap
 from matplotlib.figure import Figure
+from matplotlib.colormaps import get_cmap
 
 from .model import Classification
 from .model import Mask
@@ -90,7 +91,12 @@ def show_segmentations(
         Tuple[Figure, Axes]: Figure and Axes of the ploted results.
     """
 
-    cmap_colors = cm.get_cmap(cmap).colors
+    if not results:
+        print("A lista de resultados está vazia.")
+        return None
+    
+    cmap_colors = get_cmap(cmap).colors
+
 
     assert len(cmap_colors) >= len(
         class_names,
